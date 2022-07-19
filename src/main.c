b8fe5177fe75e567d2bedd0fbac758b066f2777f
Raffy <radoq@debian-BULLSEYE-live-builder-AMD64>
2022-07-19 15:33:10 +0800

    Implement getopt_long() function
    With the following option:
    * -t, --title = set the title of the html file.
    * -h, --help = shows help. But for now, it just display "Show some help".
    * -v, --version = shows the current version of the program. But for
                      now, it display "Show current version".

--- src/main.c
+++ src/main.c
@@ -5,12 +5,56 @@
  * Date & time:  07/10/2022-09:12:05-PM
  */
 
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
-int main() {
-    // Your code goes here...
+#define EMPTY_STRING	"\0"
+
+
+int main(int argc, char *argv[]) {
+	char *page_title = EMPTY_STRING;
+	int getopt_status, option_index = 0;
+
+	while(true) {
+        static struct option long_options[] = {
+            {"help",		no_argument,		0,  	'h'},
+            {"title",		required_argument,	0,  	't'},
+            {"version",		no_argument,		0,		'v'},
+            {0,				0,					0,		0}
+        };
+
+		opterr = 0;
+		getopt_status = getopt_long(argc, argv, ":ht:v", long_options, &option_index);
+        if (getopt_status == -1) {
+			break;
+		}
+
+		switch(getopt_status) {
+			case 'h':
+				//help();
+				printf("Show some help.\n");
+				goto skipotherprocess;
+			case 't':
+				strcpy(page_title, optarg);
+				break;
+			case 'v':
+				//version();
+				printf("Show current version.\n");
+				goto skipotherprocess;
+        }
+    }
+
+
+	skipotherprocess:;
+
     return 0;
+
+	maketh_error:;
+	return 1;
 }
 
 
